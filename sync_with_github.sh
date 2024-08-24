f143e1b1066043e984e4a32d0a6bef0395b5b02e
pqy000 <pqy_edu@163.com>
2024-08-24 14:17:55 +0800

    new update

--- sync_with_github.sh
+++ sync_with_github.sh
@@ -1,23 +1,1 @@
-#!/bin/bash
-
-REMOTE="origin"
-BRANCH="version1"
-
-git pull $REMOTE $BRANCH
-
-if [ $? -ne 0 ]; then
-    echo "Pull failed due to conflicts. Reverting local changes and trying again."
-
-    git reset --hard HEAD@{1}
-
-    git pull $REMOTE $BRANCH
-
-    if [ $? -eq 0 ]; then
-        echo "Successfully pulled from remote after reverting."
-    else
-        echo "Failed to pull from remote after reverting. Please check for issues."
-        exit 1
-    fi
-else
-    echo "Successfully pulled from remote."
-fi
+#!/bin/bashREMOTE="origin"BRANCH="version1"git pull $REMOTE $BRANCHif [ $? -ne 0 ]; then    echo "Pull failed due to conflicts. Reverting local changes and trying again."    git reset --hard HEAD@{1}    git pull $REMOTE $BRANCH    if [ $? -eq 0 ]; then        echo "Successfully pulled from remote after reverting."    else        echo "Failed to pull from remote after reverting. Please check for issues."        exit 1    fielse    echo "Successfully pulled from remote."fi

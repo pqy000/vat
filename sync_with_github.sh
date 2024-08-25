b39314d4b127537abc32df450628d2a0280ea204
pqy000 <pqy_edu@163.com>
2024-08-25 18:09:27 +0800

    new update

--- sync_with_github.sh
+++ sync_with_github.sh
@@ -2,20 +2,38 @@
 
 REMOTE="origin"
 BRANCH="version1"
+LOG_DIR="/data/qingyi/log"
+
+mkdir -p $LOG_DIR
+
+function move_conflicted_files {
+    git diff --name-only --diff-filter=U | while read file; do
+        if [ -f "$file" ]; then
+            echo "Moving conflicted file $file to $LOG_DIR"
+            mv "$file" "$LOG_DIR/"
+        fi
+    done
+}
 
 git pull $REMOTE $BRANCH
 
 if [ $? -ne 0 ]; then
-    echo "Pull failed due to conflicts. Reverting local changes and trying again."
-
+    echo "Pull failed due to conflicts. Attempting to resolve."
+    move_conflicted_files
     git reset --hard HEAD@{1}
-
     git pull $REMOTE $BRANCH
+
+    while [ $? -ne 0 ]; do
+        echo "Pull failed again. Moving conflicted files and trying to revert further."
+        move_conflicted_files
+        git reset --hard HEAD@{1}
+        git pull $REMOTE $BRANCH
+    done
 
     if [ $? -eq 0 ]; then
         echo "Successfully pulled from remote after reverting."
     else
-        echo "Failed to pull from remote after reverting. Please check for issues."
+        echo "Failed to pull from remote after multiple attempts. Please check for issues."
         exit 1
     fi
 else

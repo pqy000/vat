6525c6349fce002c6d2af86d293b6f15001df03b
pqy000 <pqy_edu@163.com>
2024-08-25 20:57:09 +0800

    new update

--- sync_with_github.sh
+++ sync_with_github.sh
@@ -2,32 +2,51 @@
 
 REMOTE="origin"
 BRANCH="version1"
-LOG_DIR="/data/qingyi/log/"
+LOG_DIR="/data/qingyi/log"
+UNTRACKED_LOG_DIR="/data/qingyi/untracked_logs"
 
+# 确保日志目录和未跟踪文件目录存在
 mkdir -p $LOG_DIR
+mkdir -p $UNTRACKED_LOG_DIR
 
 function move_conflicted_files {
     git diff --name-only --diff-filter=U | while read file; do
         if [ -f "$file" ]; then
             echo "Moving conflicted file $file to $LOG_DIR"
-            cp -rf "$file" "$LOG_DIR"
-            rm "$file"
+            mv "$file" "$LOG_DIR/"
         fi
     done
 }
+
+function move_untracked_files {
+    git ls-files --others --exclude-standard | while read file; do
+        if [ -f "$file" ]; then
+            echo "Moving untracked file $file to $UNTRACKED_LOG_DIR"
+            mv "$file" "$UNTRACKED_LOG_DIR/"
+        fi
+    done
+}
+
+move_untracked_files
 
 git pull $REMOTE $BRANCH
 
 if [ $? -ne 0 ]; then
     echo "Pull failed due to conflicts. Attempting to resolve."
+
     move_conflicted_files
+
     git reset --hard HEAD@{1}
+
     git pull $REMOTE $BRANCH
 
     while [ $? -ne 0 ]; do
         echo "Pull failed again. Moving conflicted files and trying to revert further."
+
         move_conflicted_files
+
         git reset --hard HEAD@{1}
+
         git pull $REMOTE $BRANCH
     done
 
